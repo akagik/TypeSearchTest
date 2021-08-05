3b6b83d691ad0c48cf3883ba7006304d8921e8c1
kohei <kohei.imatomi@gmail.com>
2021-08-05 10:12:15 +0900

    First Commit

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "TypeSearchTest/Human")]
+[System.Serializable]
+public class Human : ScriptableObject
+{
+    public string firstName;
+    public bool isMale;
+}
